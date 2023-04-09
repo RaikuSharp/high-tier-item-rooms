030ce9ae14fadc24fbe44f15894b4b669d6d2990
Josh <raiku541@gmail.com>
2023-04-09 12:40:27 -0700

    adjusting default settings in case it gets reset.

--- src/data/settings.ts
+++ src/data/settings.ts
@@ -1,13 +1,13 @@
 import { Difficulty, RoomType } from "isaac-typescript-definitions";
 
 export let settings = {
-  ALWAYS_ON: true,
-  SINGLE_CHOICE: false,
-  TIER_THRESHOLD: 3,
-  ITEM_COUNT: 4,
+  ALWAYS_ON: false,
+  SINGLE_CHOICE: true,
+  TIER_THRESHOLD: 0,
+  ITEM_COUNT: 1,
   SPENT_ROLL: false,
   MODES: new Set<Difficulty>(),
   ROOMS: new Set<RoomType>(),
   IS_COOP: true,
-  _VERSION: "2.6.3",
+  _VERSION: "2.6.5",
 };
